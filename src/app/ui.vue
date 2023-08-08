b9418cca6b019cef5204715c047b530e2cd3b2dd
fozilovbro <fozilovbro@gmail.com>
2023-08-08 18:22:11 +0500

    remove: root ui padding

--- src/app/ui.vue
+++ src/app/ui.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="p-6">
-    <RouterView />
-  </div>
+  <RouterView />
 </template>
 
 <script setup lang="ts"></script>
 
 <style lang="scss">
-@import "index.scss";
+@import 'index.scss';
 </style>
